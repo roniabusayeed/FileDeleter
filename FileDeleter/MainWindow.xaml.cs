25c3afd7e03017fea235664533725f5e904c6097
roniabusayeed <roni.abusayeed@gmail.com>
2022-09-04 23:25:52 +0600

    Implement erase

--- FileDeleter/MainWindow.xaml.cs
+++ FileDeleter/MainWindow.xaml.cs
@@ -11,8 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Forms;
+using System.IO;
 
 namespace FileDeleter
 {
@@ -28,10 +27,72 @@ namespace FileDeleter
 
         private void folderPathBrowseButton_Click(object sender, RoutedEventArgs e)
         {
-            using var dialog = new FolderBrowserDialog();
+            using var dialog = new System.Windows.Forms.FolderBrowserDialog();
             if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 folderPathTextBox.Text = dialog.SelectedPath.Trim();
+            }
+        }
+
+        private void updateFilenamesTextBox(List<string> filenames)
+        {
+            filenamesTextBox.Text = string.Join("\n", filenames);
+        }
+
+        private async Task deleteFilesImpl()
+        {
+            // Extract filenames.
+            var filenames = filenamesTextBox.Text
+                .Split('\n')
+                .Select(filename => filename.Trim())
+                .Where(filename => filename.Length > 0)
+                .ToList();
+
+            // Extract folder path.
+            string folderPath = folderPathTextBox.Text.Trim();
+
+            // Validate if folder path exists.
+            if (! Directory.Exists(folderPath))
+            {
+                MessageBox.Show("Folder Path Doesn't Exist", Title);
+                return; // Abort.
+            }
+
+            // Keep track of number of files successfully deleted.
+            int deletedCount = 0;
+
+            // Delete files.
+            foreach (string filename in filenames)
+            {
+                await Task.Delay(1000); // DEBUG
+                string filePath = Path.Combine(folderPath, filename);
+                if (File.Exists(filePath))
+                {
+                    try
+                    {
+                        await Task.Run(() => File.Delete(filePath));
+                        deletedCount++;
+                        updateFilenamesTextBox(filenames.Where(fn => fn != filename).ToList());
+                    } catch { }
+                }
+            }
+
+            // Feedback
+            MessageBox.Show($"{deletedCount}/{filenames.Count} file(s) deleted successfully",
+                Title);
+        }
+
+        private async void eraseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Get user confirmation.
+            var result = MessageBox.Show("Are you sure you want to delete?",
+                Title,
+                MessageBoxButton.YesNo);
+
+            // If user has confirmed, proceed with deletion.
+            if (result == MessageBoxResult.Yes)
+            {
+                await deleteFilesImpl();
             }
         }
     }
