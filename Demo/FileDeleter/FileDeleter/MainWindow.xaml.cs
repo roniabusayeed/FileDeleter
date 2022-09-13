99d4a3e03ada5c6de331394c8e87802126285966
roniabusayeed <roni.abusayeed@gmail.com>
2022-09-13 11:29:25 +0600

    Add text formatting utility

---
+++ Demo/FileDeleter/FileDeleter/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace FileDeleter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void keepFirstColumnButton_Click(object sender, RoutedEventArgs e)
+        {
+            var rows = filenamesTextBox.Text.Split('\n').ToList();
+            for (int i = 0; i < rows.Count; i++)
+            {
+                rows[i] = rows[i].Split('\t')[0];
+            }
+            filenamesTextBox.Clear();
+            foreach (var row in rows)
+            {
+                filenamesTextBox.Text += $"{row}\n";
+            }
+        }
+
+        private void deleteFirstRowButton_Click(object sender, RoutedEventArgs e)
+        {
+            var rows = filenamesTextBox.Text.Split('\n').ToList();
+            int numberOfLinesToSkip = 1;
+            rows = rows.GetRange(numberOfLinesToSkip, rows.Count - numberOfLinesToSkip);
+            filenamesTextBox.Clear();
+            foreach (var row in rows)
+            {
+                filenamesTextBox.Text += $"{row}\n";
+            }
+        }
+
+        private void folderPathBrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    folderPathTextBox.Text = dialog.SelectedPath.Trim();
+                }
+            }
+        }
+
+        private void updateFilenamesTextBox(List<string> filenames)
+        {
+            filenamesTextBox.Text = string.Join("\n", filenames);
+        }
+        private List<string> extractFilenames()
+        {
+            return filenamesTextBox.Text
+                .Split('\n')
+                .Select(filename => filename.Trim())
+                .Where(filename => filename.Length > 0)
+                .ToList();
+        }
+        private async Task deleteFilesImpl()
+        {
+            // Extract filenames.
+            var filenames = extractFilenames();
+
+            // Extract folder path.
+            string folderPath = folderPathTextBox.Text.Trim();
+
+            // Validate if folder path exists.
+            if (!Directory.Exists(folderPath))
+            {
+                MessageBox.Show("Folder Path Doesn't Exist", Title);
+                return; // Abort.
+            }
+
+            // Keep track of number of files successfully deleted.
+            int deletedCount = 0;
+
+            // Delete files.
+            foreach (string filename in filenames)
+            {
+                string filePath = Path.Combine(folderPath, filename);
+                if (File.Exists(filePath))
+                {
+                    try
+                    {
+                        await Task.Run(() => File.Delete(filePath));
+                        deletedCount++;
+                        updateFilenamesTextBox(
+                            extractFilenames()
+                            .Where(fn => fn != filename)
+                            .ToList()
+                        );
+                    }
+                    catch { }
+                }
+            }
+
+            // Feedback
+            MessageBox.Show($"{deletedCount}/{filenames.Count} file(s) deleted successfully",
+                Title);
+        }
+
+        private async void eraseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Get user confirmation.
+            var result = MessageBox.Show("Are you sure you want to delete?",
+                Title,
+                MessageBoxButton.YesNo);
+
+            // If user has confirmed, proceed with deletion.
+            if (result == MessageBoxResult.Yes)
+            {
+                await deleteFilesImpl();
+            }
+        }
+
+        private void resetButton_Click(object sender, RoutedEventArgs e)
+        {
+            folderPathTextBox.Clear();
+            filenamesTextBox.Clear();
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
